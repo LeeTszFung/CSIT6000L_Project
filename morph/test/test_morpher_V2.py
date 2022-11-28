ae0cb7d98d0fe2e976015e8a56c6af108feb40fc
LeeTszFung <77471196+LeeTszFung@users.noreply.github.com>
2022-11-28 19:53:35 +0800

    Update test_morpher_V2.py

--- morph/test/test_morpher_V2.py
+++ morph/test/test_morpher_V2.py
@@ -7,7 +7,7 @@ import cv2
 if __name__ == '__main__':
     size = (600, 500)
 	mtcnnModel.Get_input()
-	mtcnnModel.Aline_celebrity()
+	mtcnnModel.Align_celebrity()
 	mtcnnModel.generate_user_faces(mtcnnModel.input_image)
 	mtcnnModel.image_mapping()
 	for face in mtcnnModel.Possible_face: 
