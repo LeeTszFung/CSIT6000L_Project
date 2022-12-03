e3e7f851a547fe262a19b02ce4b7a74198bfdc5f
theredlee <49631756+theredlee@users.noreply.github.com>
2022-12-04 02:09:23 +0800

    Update test_mtcnn for UI interaction

--- mtcnn/test_mtcnn.py
+++ mtcnn/test_mtcnn.py
@@ -1,12 +1,15 @@
 from mtcnnModel import mtcnnModel
-from VGGFaceModel import VGGFaceModel
-import cv2
+import os
+import shutil
+from PIL import Image
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 if __name__ == '__main__':
 	size = (600, 500)
 	model = mtcnnModel()
 	model.Get_input()
 	#model.Align_celebrity()
+	
 	for input_image in model.input_file:
 		model.generate_user_faces(input_image)
 		model.image_mapping()
@@ -17,3 +20,10 @@ if __name__ == '__main__':
 		Image_path = 'Image'
 		Celebrity_filename = Image_path+'/'+user.matched
 		print(Celebrity_filename)
+		# Save to most similar path
+		src_path = Celebrity_filename
+		dst_path = "../morpher/test/dst/"
+		# print(user.face_image)
+# 		im = Image.fromarray(user.face_image)
+# 		im.save(dst_path+"dst.jpg")
+		# shutil.copy(src_path, dst_path)
